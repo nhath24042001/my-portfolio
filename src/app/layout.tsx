8728dea69654c88cf7fe356e06aa9fad4725e613
nhath24042001 <nhath24042001@gmail.com>
2025-06-08 22:37:28 +0700

    refactor: update font usage in layout and adjust header padding for better aesthetics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google'
+// import { Open_Sans } from 'next/font/google'
+import { Roboto_Mono } from "next/font/google";
 
 import Header from "@/components/shared/Header";
 
-export const inter = Inter({ subsets: ['latin'], display: 'swap' })
+// export const inter = Inter({ subsets: ['latin'], display: 'swap' })
+
+export const font = Roboto_Mono({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '700', ],
+  variable: '--font-lato',
+})
 
 export const metadata: Metadata = {
   title: "Nhat Huy - Portfolio",
@@ -17,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en" className={font.className}>
       <body>
         {/* <AnimatedBackground /> */}
         {/* <CustomCursor /> */}
