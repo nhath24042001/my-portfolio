39666415e61ca99205543f13a5c10c4bd8499fcc
nhath24042001 <nhath24042001@gmail.com>
2025-06-11 20:40:04 +0700

    feat: add remote image patterns to next.config for enhanced image loading

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,34 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.viblo.asia',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static-00.iconduck.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
